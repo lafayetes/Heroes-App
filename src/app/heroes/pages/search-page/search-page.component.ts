f6f0d2bf8bf70af88a029cd55732d3337d34ab9d
Diego La Faye <lafayediego@gmail.com.com>
2024-01-31 16:07:45 -0400

    concluded UI for the heroes app

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { HeroesService } from '../../services/heroes.service';
+import { Hero } from '../../interfaces/hero.interface';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -7,4 +11,29 @@ import { Component } from '@angular/core';
 })
 export class SearchPageComponent {
 
+  public searchInput =new FormControl('');
+  public heroes:Hero[]=[]
+  public selectedHero?:Hero;
+  constructor(private heroesService:HeroesService){}
+
+  searchHero(){
+    console.log(this.searchInput.value);
+
+    const value:string = this.searchInput.value||'';
+    this.heroesService.getHeroSuggestions(value,5)
+    .subscribe(heroes => this.heroes = heroes );
+
+
+  }
+
+  onSelectedOption(event:MatAutocompleteSelectedEvent){
+    if (!event.option.value) {
+      this.selectedHero = undefined;
+      return;
+    }else{
+      const hero = event.option.value;
+      this.searchInput.setValue(hero.superhero);
+      this.selectedHero = hero;
+    }
+  }
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../enviroments/environments';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class HeroesService {
 
   private baseUrl = environments.baseUrl;
@@ -12,12 +12,24 @@ export class HeroesService {
   constructor(private http: HttpClient) { }
 
 
-//Es bueno siempre mantener el estandar de colocar el tipo de retorno que tendra y siempre manejar interfaces
-  getHeroes():Observable<Hero[]>{
+  //Es bueno siempre mantener el estandar de colocar el tipo de retorno que tendra y siempre manejar interfaces
+  getHeroes(): Observable<Hero[]> {
 
 
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
 
+  }
+
+  getHeroById(id: string): Observable<Hero | undefined> {
+
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError(error => of(undefined))
+      );
+  }
+
+  getHeroSuggestions(query:string,limit:number):Observable<Hero[]>{
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=${limit}`)
   }
 
 }
